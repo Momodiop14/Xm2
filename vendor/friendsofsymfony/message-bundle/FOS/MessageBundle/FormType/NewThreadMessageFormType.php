984721f5a4792bb0d073e6a5d23893664d401e8c
Momodiop14 <mad@esp.sn>
2015-09-06 17:33:13 +0200

    revision

---
+++ vendor/friendsofsymfony/message-bundle/FOS/MessageBundle/FormType/NewThreadMessageFormType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace FOS\MessageBundle\FormType;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Message form type for starting a new conversation
+ *
+ * @author Thibault Duplessis <thibault.duplessis@gmail.com>
+ */
+class NewThreadMessageFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('destinataire', 'fos_user_username')
+            ->add('sujet', 'text')
+            ->add('contenu', 'textarea');
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'intention'  => 'message',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'fos_message_new_thread';
+    }
+}
