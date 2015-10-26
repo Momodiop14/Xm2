7c17720ce9052705004f9e51c8a2386d9c5f8e28
Momodiop14 <mad@esp.sn>
2015-10-26 21:54:15 +0100

    migration v 2.7 | persister covoiturage | activation FosCommentBundle

--- script_cache.sh
+++
@@ -1,5 +0,0 @@
-sudo chmod -R 777 /media/www-data/XarrMatt/app/cache
-sudo rm -rf /media/www-data/XarrMatt/app/cache
-app/console cache:clear --env=dev
-app/console cache:clear --env=prod
-sudo chmod -R 777 /media/www-data/XarrMatt/app/cache
